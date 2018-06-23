3d42ece956a3b2f4a720b20731b5dd0297a3e7e8
Julia Pottinger <jpottinger@qualityworkscg.com>
2018-06-23 12:42:20 -0700

    Added temporary register component

---
+++ src/components/register.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class register extends Component {
+    render() {
+        return (
+            <div>
+                This is the Register Component
+            </div>
+        );
+    }
+}
+
+export default register;
